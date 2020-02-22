5f78bb501d9c4b047111e96d0bd0aa509cc7e226
mambarinka <54846120+mambarinka@users.noreply.github.com>
2020-02-22 17:31:25 +0400

    подключает js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 var link = document.querySelector("section.hotel-search .button-brown");
-
 var popup = document.querySelector(".modal");
-var open = popup.querySelector("section.hotel-search .button-brown");
-
 var form = popup.querySelector("form");
 var chekin = popup.querySelector("[name=check-in]");
 var chekout = popup.querySelector("[name=check-out]");
@@ -10,46 +7,55 @@ var adults = popup.querySelector("[name=adults]");
 var children = popup.querySelector("[name=children]");
 
 var isStorageSupport = true;
-var storage = "";
+var storageAdults = "";
+var storageChildren = "";
+
+
+/* проверка на безопасное соединение в localStorage */
 
 try {
-  storage = localStorage.getItem("adults");
+    storageAdults = localStorage.getItem("adults");
 } catch (err) {
-  isStorageSupport = false;
+    isStorageSupport = false;
 }
 
 try {
-  storage = localStorage.getItem("children");
+    storageChildren = localStorage.getItem("children");
 } catch (err) {
-  isStorageSupport = false;
+    isStorageSupport = false;
 }
+
+/* описание обработчика событий по click */
 
 link.addEventListener("click", function (evt) {
-  popup.classList.add("modal-close");
-  evt.preventDefault();
-  if (storage) {
-    adults.value = storage;
-  }
-
-  if (storage) {
-    children.value = storage;
-  }
+    evt.preventDefault();
+    popup.classList.toggle("modal-close");
+    popup.classList.add("modal-bounce");
+    popup.classList.remove("modal-error");
+    chekin.focus()
+    if (storageAdults && storageChildren) {
+        adults.value = storageAdults;
+        children.value = storageChildren;
+    }
 });
 
-
-open.addEventListener("click", function (evt) {
-  popup.classList.remove("modal-close");
-});
-
+/* проверка на заполнение некоторых полей ввода */
 
 form.addEventListener("submit", function (evt) {
-  if (!chekin.value || !checkout.value || !adults.value || !children.value) {
-    evt.preventDefault();
-    console.log("Нужно ввести дату заезда, дату выезда, заполнить количество взрослых и детей");
-  }
 
-  if (isStorageSupport) {
-    localStorage.setItem("adults", adults.value);
-    localStorage.setItem("children", children.value);
-  }
+    if (!chekin.value || !checkout.value) {
+        evt.preventDefault();
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
+        console.log("Нужно ввести дату заезда и дату выезда");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("adults", adults.value);
+            localStorage.setItem("children", children.value);
+        }
+    }
 });
+
+
+
